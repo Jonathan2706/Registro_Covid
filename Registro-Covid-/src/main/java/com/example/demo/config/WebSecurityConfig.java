7e8dddf1d5f3977f12ac0498af8c08468fdfa9bf
Vinicio <vini13perla11@gmail.com>
2020-12-10 18:23:03 -0500

    Complemento Login
    Pd: hay un error de la app debe ser por la falta de la dependencia.

--- Registro-Covid-/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ Registro-Covid-/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.example.demo.service.UserDetailsServiceImpl;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
