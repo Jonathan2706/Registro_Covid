16afb125da5dcd376433f1713fb11c277738d9d9
Vinicio <vini13perla11@gmail.com>
2020-12-14 20:03:58 -0500

    Codigo HTML del Login
    Y un complemento del formulario.

--- src/main/java/com/spring/covid/service/UserDetailsServiceImpl.java
+++ src/main/java/com/spring/covid/service/UserDetailsServiceImpl.java
@@ -1,7 +1,7 @@
 package com.spring.covid.service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,33 +11,32 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.spring.covid.entity.Authority;
+import com.spring.covid.entity.Role;
 import com.spring.covid.repository.UserRepository;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
+	@Autowired
     UserRepository userRepository;
 	
-    @Override
-     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
-     //Buscar el usuario con el repositorio y si no existe lanzar una exepcion
-     com.spring.covid.entity.User appUser = 
-                 userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("No existe usuario"));
+		com.spring.covid.entity.User appUser = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Login Username Invalido."));
 		
-    //Mapear nuestra lista de Authority con la de spring security 
-    List grantList = new ArrayList();
-    for (Authority authority: appUser.getAuthority()) {
-        // ROLE_USER, ROLE_ADMIN,..
-        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(authority.getAuthority());
+		Set<GrantedAuthority> grantList = new HashSet<GrantedAuthority>(); 
+		for (Role role: appUser.getRoles()) {
+			GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getDescription());
             grantList.add(grantedAuthority);
-    }
+		}
+		UserDetails user = (UserDetails) new User(username,appUser.getPassword(),grantList);
 		
-    //Crear El objeto UserDetails que va a ir en sesion y retornarlo.
-    UserDetails user = (UserDetails) new User(appUser.getUsername(), appUser.getPassword(), grantList);
-         return user;
-    }
+		return user;
+	}
+	
+
 }
